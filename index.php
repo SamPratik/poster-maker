cb8a8dc32322a419db4e8b095deb4c1c314dcd2c
SamPratik <pratik.anwar@gmail.com>
2018-03-15 22:24:42 +0600

    navbar issues fixed

--- index.php
+++ index.php
@@ -21,9 +21,9 @@
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">TeTeTe</a>
+      <a class="navbar-brand" href="#">TeTeTa</a>
     </div>
-    <ul class="nav navbar-nav navbar-right" style="color:white;font-size:24px;font-weight:bold;right:30px;">
+    <ul class="nav navbar-nav navbar-right" style="color:white;font-size:30px;position:absolute;right:50px;">
       Creative Center
     </ul>
   </div>
